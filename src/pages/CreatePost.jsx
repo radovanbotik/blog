3e1911d335faaf96c3a8d80de7d123d53502d88e
radovanbotik <radovanbotik@gmail.com>
2022-11-17 17:36:57 +0100

    ''

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function CreatePost() {
-  return <div>CreatePost</div>;
+  return (
+    <div>
+      <h2>create a new post</h2>
+      <form action="" style={{ display: "grid", placeItems: "center" }}>
+        <label htmlFor="title">title</label>
+        <input type="text" id="title" name="title" />
+        <label htmlFor="body">message body</label>
+        <textarea name="body" id="body"></textarea>
+        <button>create a post</button>
+      </form>
+    </div>
+  );
 }
